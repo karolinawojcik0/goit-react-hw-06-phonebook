ccbbf9b56cfff777522f310ece5d675bf283c7e9
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-05-16 22:02:12 +0200

    upd.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsTitle, Container, Title } from './App.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', tel: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', tel: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', tel: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', tel: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = newContact => {
+    const isExistingContact = contacts.some(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (isExistingContact) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+
+    setContacts([...contacts, newContact]);
+  };
+
+  const removeContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm addContact={addContact} />
+      <ContactsTitle>Contacts list:</ContactsTitle>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={filteredContacts} removeContact={removeContact} />
+    </Container>
   );
 };
